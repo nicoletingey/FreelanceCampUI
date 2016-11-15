4e107636b9360ded5e7e2ee99647e83f1388a1c8
Nicole Tingey <nicole.tingey1@gmail.com>
2016-11-15 16:04:45 -0700

    Updated version

---
+++ app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-broswer';
+
+import { HomepageComponent } from './homepage.component';
+@NgModule({
+	imports: [
+		BroswerModule
+	],
+	declarations: [
+		HomepageComponent
+	],
+	bootstrap [
+		HomepageComponent
+	]
+})
+export class AppModule {}
