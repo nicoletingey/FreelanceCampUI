3fdbb3af7fdc58d0a402f53eb9673d6895357f16
Nicole Tingey <nicole.tingey1@gmail.com>
2016-11-16 12:17:29 -0700

    Creating mock data

--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { Document } from './document';
 
-@Component ({
+@Component({
 	moduleId: module.id,
 	selector: 'documents',
 	templateUrl: 'documents.component.html'
 })
 
 export class DocumentsComponent {
-
+	documents: Document[] = [
+		{
+			title: "My First Doc",
+			description: 'assdfasdadf asdasfasf',
+			file_url: 'http://google.com',
+			updated_at: '11/11/16',
+			image_url: 'http://google.com',
+		},
+		{
+			title: "My Second Doc",
+			description: 'assdfasdadf asdasfasf',
+			file_url: 'http://google.com',
+			updated_at: '11/11/16',
+			image_url: 'http://google.com',
+		},
+		{
+			title: "My Last Doc",
+			description: 'assdfasdadf asdasfasf',
+			file_url: 'http://google.com',
+			updated_at: '11/11/16',
+			image_url: 'http://google.com',
+		}
+	]
 }
